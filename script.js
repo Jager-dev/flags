8b20dc9bc2cb57b618417ff04d99d0be4ffc5d01
Ulan Zhakypov <zhakypovu@gmail.com>
2021-08-13 08:26:50 +0300

    finish

---
+++ script.js
@@ -0,0 +1,17 @@
+const row = document.querySelector(".row")
+
+
+fetch("https://restcountries.eu/rest/v2/all")
+  .then((res) => res.json())
+  .then(data => data.forEach(el => {
+    row.innerHTML += `<div class="col-3">
+    <img src="${el.flag}" alt="" class="flags">
+      <div>
+      <ul>
+      <li>Country: ${el.name}</li>
+      <li>Population: ${el.population}</li>
+      <li>Capital: ${el.capital}</li>
+      </ul>
+      </div>
+    </div>`
+  }))
